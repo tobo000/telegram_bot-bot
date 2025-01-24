73229fa8f42d7666a254f0fdd820f25af36c77c3
TOBO Script VVIP <141430795+tobo000@users.noreply.github.com>
2025-01-24 18:16:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python
 
+import telebot
+from keep_alive import keep_alive
+
+keep_alive()
+
+bot_token = "7349804202:AAEWcMMSPQCO-YLA7gcgMK3kQ-ZVzt8nsuo" # add your telegram bot token
+
+bot = telebot.TeleBot(bot_token)
+
+@bot.message_handler(commands=['start'])
+
+def wlcm_msg(message):
+    wlcm_text = """  WELLCOME TO CHAT ID BOT 
+    Send /id command to Get your Id
+created by @bigboss_global_trade""" # add your username 
+    bot.reply_to(message, wlcm_text)
+
+@bot.message_handler(commands=['id'])
+def send_id(message):
+    chat_id = message.chat.id 
+    bot.reply_to(message, f"Your Id: {chat_id}")
+bot.polling()
